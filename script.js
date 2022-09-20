5205f5450fa8f99cf1e97e442df7249dea435b88
Thaqif Azfar <thaqifazfar15@gmail.com>
2022-09-20 10:21:43 +0800

    feature: minimzed book cards

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let myLibrary = [];
 
-function Book(title, author, pages, isRead, img) {
-  this.title = title.toString();
-  this.author = author.toString();
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
   this.pages = pages;
   this.isRead = isRead;
-  this.img = img;
+  // this.img = img;
 }
 
-const Naruto = new Book("Naruto", "Kishimoto", "240", false);
+const Naruto = new Book("Naruto", "Kishimoto", "240", true);
 addBookToLibrary(Naruto);
 
 function addBookToLibrary(book) {
@@ -28,44 +28,96 @@ turnOffOverlay = () => {
 };
 
 // loop through array, and append all book to container
-for (let i = 0; i < myLibrary.length; i++) {
-  createBookCards(myLibrary[i]);
+function checkLibraryForBooks() {
+  const bookContainer = document.getElementsByClassName("book-container");
+  bookContainer[0].innerHTML = "";
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    createBookCards(myLibrary[i], i);
+  }
 }
 
-function createBookCards(book) {
+checkLibraryForBooks();
+
+function createBookCards(book, indexNumber) {
   const bookContainer = document.getElementsByClassName("book-container");
   const divMain = document.createElement("div");
-  const img = document.createElement("img");
-  const div1 = document.createElement("div");
-  const div2 = document.createElement("div");
-  const div3 = document.createElement("div");
+  divMain.dataset.indexNumber = indexNumber;
 
-  div1.classList.add("book-info");
-  div1.classList.add("flex");
+  let html = `
+  <img class="book-img" src="/img/naurto-cover.webp" alt="">
+  <div class="book-info flex">
+      <div class="book-info-left flex">
+          <h3>${book.title}</h3>
+          <span>by ${book.author}</span>
+      </div>
+      <div class="book-info-right flex">
+          <h7>${book.pages} Pages</h7>
+      </div>
+      
+  </div>
+  <div class="flex book-read-status"> 
+      <span>Finish Reading</span>
+      <label class="switch">
+          <input type="checkbox" ${book.isRead ? "checked" : ""}>
+          <span class="slider round"></span>
+      </label>
+      </label>
+  </div>
+  <div class="book-buttons-container">
+      <button class="button"> Edit</button>
+      <button class="button">Delete</button>
+  </div>           `;
 
-  const div1__div__left = document.createElement("div");
-  div1__div__left.classList.add("book-info-left");
-  div1__div__left.classList.add("flex");
-
-  const div1__left__h3 = document.createElement("h3");
-  div1__left__h3.innerHTML = book.title;
-
-  const div1__left__span = document.createElement("span");
-  div1__left__span.innerHTML = `by ${book.author}`;
-
-  div1__div__left.appendChild(div1__left__h3);
-  div1__div__left.appendChild(div1__left__span);
-  div1.appendChild(div1__div__left);
-
-  div2.classList.add("book-read-status");
-  div2.classList.add("flex");
-
-  div3.classList.add("book-button-container");
-
-  divMain.appendChild(img);
-  divMain.appendChild(div1);
-  divMain.appendChild(div2);
-  divMain.appendChild(div3);
-
+  divMain.innerHTML = html;
   bookContainer[0].appendChild(divMain);
+}
+
+const formContainer = document.getElementsByClassName("form-container");
+formContainer[0].addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const bookTitle = document.getElementById("book-title");
+  const bookAuthor = document.getElementById("book-author");
+  const bookPages = document.getElementById("book-pages");
+  const bookIsRead = document.getElementById("book-is-read");
+
+  if (
+    (bookTitle.value == "") |
+    (bookAuthor.value == "") |
+    (bookPages.value == "")
+  ) {
+    return;
+  }
+
+  let newBook = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPages.value,
+    bookIsRead.checked
+  );
+  addBookToLibrary(newBook);
+  checkLibraryForBooks();
+
+  //reset form
+  bookTitle.value = "";
+  bookAuthor.value = "";
+  bookPages.value = "";
+  bookIsRead.checked = false;
+
+  turnOffOverlay();
+});
+
+const bookCover = document.getElementsByClassName("book-img");
+
+function showImage() {
+  for (let i = 0; i < bookCover.length; i++) {
+    bookCover[0].style.maxHeight = "22em";
+  }
+}
+
+function hideImage() {
+  for (let i = 0; i < bookCover.length; i++) {
+    bookCover[0].style.maxHeight = "0";
+  }
 }
